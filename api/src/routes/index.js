d711b457360bb3c395729a9a9402bf7412c42b8d
Diego PS <dpahuas@gmail.com>
2022-03-08 14:14:38 -0600

    antes del error

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
-const router = Router();
+const app = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-
-module.exports = router;
+const videogame = require("./videogame");
+const videogames = require("./videogames");
+const genre = require("/genre");
+app.use("/videgame", videogame);
+app.use("/videogames", videogames);
+app.use("/genre", genre);
+module.exports = app;
