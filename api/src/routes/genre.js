4ccdd4e026d17331e0af2e56450c9fef2ea2f44b
Diego PS <dpahuas@gmail.com>
2022-03-08 14:15:37 -0600

    despues del error pero antes del otro error

--- api/src/routes/genre.js
+++ api/src/routes/genre.js
@@ -1,6 +1,2 @@
 const { Router } = require("express");
 const router = Router();
-
-[ ] GET /genres:
-Obtener todos los tipos de géneros de videojuegos posibles
-En una primera instancia deberán traerlos desde rawg y guardarlos en su propia base de datos y luego ya utilizarlos desde allí
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,8 +1,2 @@
 const { Router } = require("express");
 const router = Router();
-[ ] GET /videogames:
-Obtener un listado de los videojuegos
-Debe devolver solo los datos necesarios para la ruta principal
-[ ] GET /videogames?name="...":
-Obtener un listado de las primeros 15 videojuegos que contengan la palabra ingresada como query parameter
-Si no existe ningún videojuego mostrar un mensaje adecuado
