ee51a22ee2332f306b0747c36a9ac998692ffc29
victor rodriguez <victornisaelrodriguez@gmail.com>
2022-03-09 18:39:28 -0500

    GET videogame/:videogameId falta dar el formato al json

--- api/src/routes/videogame.js
+++ api/src/routes/videogame.js
@@ -1,6 +1,8 @@
-const { Router } = require("express");
+const { default: axios } = require("axios");
+const { Router, response } = require("express");
 const { Genre, Videogame, Platform } = require("../db");
 const router = Router();
+const { API_KEY } = process.env;
 router.post("/", async (req, res) => {
   const {
     name,
@@ -39,6 +41,65 @@ router.post("/", async (req, res) => {
       }) */
   } catch (error) {
     console.log("ALGO FALLO EN EL POST", error);
+  }
+});
+
+router.get("/:videogameId", async (req, res) => {
+  const { videogameId } = req.params;
+  //  if( videogameId >)
+  //console.log(videogameId.length);
+  const formatter = new Intl.ListFormat("es", {
+    style: "long",
+    type: "conjunction",
+  });
+
+  if (videogameId.length < 6) {
+    const videogame = await axios.get(
+      `https://api.rawg.io/api/games/${videogameId}?key=${API_KEY}`
+    );
+    let {
+      name,
+      description,
+      background_image,
+      rating,
+      released,
+      genres,
+      platforms,
+    } = videogame.data;
+
+    const gameDetails = {
+      videogameId,
+      name,
+      description: description.replace(/(<([^>]+)>)/gi, " "),
+      background_image,
+      rating,
+      released,
+      genre: formatter.format(genres.map((e) => e.name)),
+      platforms: formatter.format(platforms.map((e) => e.platform.name)),
+    };
+    res.send(gameDetails);
+  } else {
+    let videogame = await Videogame.findByPk(videogameId, {
+      include: [
+        {
+          model: Genre,
+          through: { attributes: [name] },
+        },
+        {
+          model: Platform,
+          through: { attributes: [] },
+        },
+      ],
+    });
+
+    /*     let videogame = await Videogame.findOne({
+      where: {
+        id: videogameId,
+      },
+      include: Genre,
+      attributes: [Genre.name],
+    }); */
+    res.status(200).json(videogame);
   }
 });
 module.exports = router;
